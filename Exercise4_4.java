9170af886964939d1835f9b4705756aaf3b02cd5
ceylandagilmaa <dagilmaceylan@gmail.com>
2025-12-03 22:41:33 +0300

    Add comments to Exercise4_4.java methods

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -1,21 +1,21 @@
-public class Exercise4_4 {
+public class Exercise4_4 {                                  // Sınıf tanımı
 
-    public static int getNumber() {
-        return 42;
+    public static int getNumber() {                         // int döndüren metot
+        return 42;                                          // 42 değerini döndürür
     }
 
-    public static void sayHello() {
-        System.out.println("hello");
+    public static void sayHello() {                         // void metodu
+        System.out.println("hello");                        // "hello" yazdırır
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {                // Programın başlangıç noktası
 
         // 1) Donus degerini kullanmayin -> ne oluyor?
-        getNumber();
+        getNumber();                                        // Değer döner, fakat kullanılmadığı için ekrana bir şey yazılmaz
 
         // 2) void metodu ifade icinde kullanin -> ne oluyor?
-        // System.out.println(sayHello() + 7);
+        // System.out.println(sayHello() + 7);              // HATA! sayHello() void olduğu için + 7 ile kullanılamaz
 
-        // Cevaplarinizi yorum olarak ekleyin.
+        // Cevaplarinizi yorum olarak ekleyin.              // Öğrenciden cevap istenen bölüm
     }
 }
