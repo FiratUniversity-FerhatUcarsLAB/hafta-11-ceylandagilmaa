fcf3154d60fecfb5c2219a2700d1962b69af1de4
ceylandagilmaa <dagilmaceylan@gmail.com>
2025-12-03 22:33:19 +0300

    Update Exercise4_1.java

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -11,4 +11,26 @@ public class Exercise4_1 {
     public static void main(String[] args) {
         // TODO: test methods
     }
+}public class DatePrinter {
+
+    // American format: Day, Month DayNumber, Year
+    public static void printAmerican(String day, String month, int dayNumber, int year) {
+        System.out.println(day + ", " + month + " " + dayNumber + ", " + year);
+    }
+
+    // European format: Day DayNumber Month Year
+    public static void printEuropean(String day, int dayNumber, String month, int year) {
+        System.out.println(day + " " + dayNumber + " " + month + " " + year);
+    }
+
+    public static void main(String[] args) {
+
+        String day = "Monday";
+        String month = "January";
+        int dayNumber = 15;
+        int year = 2025;
+
+        printAmerican(day, month, dayNumber, year);
+        printEuropean(day, dayNumber, month, year);
+    }
 }
